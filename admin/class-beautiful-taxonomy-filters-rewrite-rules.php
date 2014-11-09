785029a9391860cec48da49f48f95faed6a2dfdc
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-11-08 18:37:02 -0700

    Use a post type’s has_archive for rewrites if it’s specified.

--- admin/class-beautiful-taxonomy-filters-rewrite-rules.php
+++ admin/class-beautiful-taxonomy-filters-rewrite-rules.php
@@ -37,12 +37,12 @@ class Beautiful_Taxonomy_Filters_Rewrite_Rules {
 	 * @since    1.0.0
 	 */
 	public function generate_rewrite_rules( $post_type, $excluded_taxonomies, $query_vars = array() ) {
-		
+
 	    global $wp_rewrite;
-	 
+
 	    if( ! is_object( $post_type ) )
 	        $post_type = get_post_type_object( $post_type );
-	 
+
 	    $new_rewrite_rules = array();
 	    $taxonomies = get_object_taxonomies( $post_type->name, 'objects' );
 	    // Add taxonomy filters to the query vars array
@@ -57,31 +57,40 @@ class Beautiful_Taxonomy_Filters_Rewrite_Rules {
 	    }
 	    // Loop over all the possible combinations of the query vars
 	    for( $i = 1; $i <= count( $query_vars );  $i++ ) {
-	 
-	        $new_rewrite_rule =  $post_type->rewrite['slug'] . '/';
+
+			if ( true === $post_type->has_archive ) {
+
+				$new_rewrite_rule =  $post_type->rewrite['slug'] . '/';
+
+			} else {
+
+				$new_rewrite_rule =  $post_type->has_archive . '/';
+
+			}
+
 	        $new_query_string = 'index.php?post_type=' . $post_type->name;
-	 
+
 	        // Prepend the rewrites & queries
 	        for( $n = 1; $n <= $i; $n++ ) {
 	            $new_rewrite_rule .= '(' . implode( '|', $query_vars ) . ')/(.+?)/';
 	            $new_query_string .= '&' . $wp_rewrite->preg_index( $n * 2 - 1 ) . '=' . $wp_rewrite->preg_index( $n * 2 );
 	        }
-	 
+
 	        // Allow paging of filtered post type - WordPress expects 'page' in the URL but uses 'paged' in the query string so paging doesn't fit into our regex
 	        $new_paged_rewrite_rule = $new_rewrite_rule . 'page/([0-9]{1,})/';
 	        $new_paged_query_string = $new_query_string . '&paged=' . $wp_rewrite->preg_index( $i * 2 + 1 );
-	 
+
 	        // Make the trailing backslash optional
 	        $new_paged_rewrite_rule = $new_paged_rewrite_rule . '?$';
 	        $new_rewrite_rule = $new_rewrite_rule . '?$';
-	 
+
 	        // Add the new rewrites
 	        $new_rewrite_rules = array( $new_paged_rewrite_rule => $new_paged_query_string,
 	                                    $new_rewrite_rule       => $new_query_string )
 	                             + $new_rewrite_rules;
 	    }
 	    return $new_rewrite_rules;
-	}	
+	}
 
 }
 ?>
--- public/class-beautiful-taxonomy-filters-public.php
+++ public/class-beautiful-taxonomy-filters-public.php
@@ -51,10 +51,10 @@ class Beautiful_Taxonomy_Filters_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_styles() {
-		
+
 		//the basic stylesheet that should always be loaded! For select2 to display properly
 		wp_enqueue_style( 'select2', plugin_dir_url( __FILE__ ) . 'css/select2.css', array(), $this->version, 'all' );
-		
+
 		//Get user selected style
 		$selected_style = get_option('beautiful_taxonomy_filters_styles');
 		switch ($selected_style){
@@ -67,7 +67,7 @@ class Beautiful_Taxonomy_Filters_Public {
 			case 'dark-material':
 				wp_enqueue_style( $this->name, plugin_dir_url( __FILE__ ) . 'css/beautiful-taxonomy-filters-dark-material.css', array(), $this->version, 'all' );
 				break;
-			
+
 		}
 
 	}
@@ -78,28 +78,28 @@ class Beautiful_Taxonomy_Filters_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-		
+
 		wp_enqueue_script( 'select2', plugin_dir_url( __FILE__ ) . 'js/select2/select2.min.js', array( 'jquery' ), $this->version, false );
 		wp_enqueue_script( $this->name, plugin_dir_url( __FILE__ ) . 'js/beautiful-taxonomy-filters-public.js', array( 'select2' ), $this->version, false );
 
 	}
-	
-	
+
+
 	/**
 	 * Output any overriding CSS by the user from plugin settings
 	 *
 	 * @since    1.0.0
 	 */
 	public function custom_css() {
-		
+
 		$custom_css = get_option('beautiful_taxonomy_filters_custom_css');
 		if($custom_css)
 			echo '<style type="text/css">' . $custom_css . '</style>';
 
 	}
-	
+
 	/**
-	* Appends the already existing GET parameters to the url. 
+	* Appends the already existing GET parameters to the url.
 	* This allows for custom parameters to carry on to the filtered page
 	* @since 1.0.0
 	*/
@@ -115,8 +115,8 @@ class Beautiful_Taxonomy_Filters_Public {
 		}
 		return $new_url;
 	}
-	
-	
+
+
 	/**
 	* Runs on template_include filter. Check for $POST values coming from the filter and add them to the url
 	* Also check for custom GET parameters and reattach them to the url to support combination with other functionalities
@@ -127,7 +127,7 @@ class Beautiful_Taxonomy_Filters_Public {
 		$referer = (isset($_POST['site-url']) ? $_POST['site-url'] : false);
 		if(!$referer)
 			return;
-		
+
 		//get current post type archive
 		if(isset($_POST['post_type']) && $_POST['post_type'] != ''){
 			$current_post_type = $_POST['post_type'];
@@ -144,52 +144,63 @@ class Beautiful_Taxonomy_Filters_Public {
 					//didnt find the post type in the template, fall back to the wp_query!
 					global $wp_query;
 					if($wp_query->query['post_type'] != ''){
-						$current_post_type = $wp_query->query['post_type'];	
+						$current_post_type = $wp_query->query['post_type'];
 					}
 				}
 			}
 		}
-		
-		
+
+		$post_type_obj  = get_post_type_object( $current_post_type );
+
+		if ( get_option( 'permalink_structure' ) && is_array( $post_type_obj->rewrite ) ) {
+
+			$post_type_slug = ( true === $post_type_obj->has_archive ) ? $post_type_obj->rewrite['slug'] : $post_type_obj->has_archive;
+
+		} else {
+
+			$post_type_slug = $current_post_type;
+
+		}
+
 		//base url
-		$new_url = $referer . '/' . $current_post_type . '/';
-		
+		$new_url = $referer . '/' . $post_type_slug . '/';
+
 		//Get the taxonomies of the current post type
 		$current_taxonomies = get_object_taxonomies($current_post_type, 'objects');
 		if($current_taxonomies){
 			foreach($current_taxonomies as $key => $value){
-				
-				//check for each taxonomy as a $_POST variable. 
+
+				//check for each taxonomy as a $_POST variable.
 				//If it exists we want to append it along with the value (term) it has.
 				$term = (isset($_POST['select-'.$key]) ? $_POST['select-'.$key] : false);
 				if($term){
 					$new_url .= $key . '/' . $term . '/';
 				}
-				
+
 			}
 		}
-		
+
 		$new_url = $this->append_get_parameters($new_url);
-		
+
 		//perform a redirect to the new filtered url
 		wp_redirect($new_url);
 		exit;
 	}
-	
-	
+
+
 	/**
-	* Public function to return the filters for the current post type archive. 
+	* Public function to return the filters for the current post type archive.
 	*
 	* @since 1.0.0
 	*/
 	public static function beautiful_filters(){
 		//Fetch the plugins options
 		//Apply filters on them to let users modify the options before they're being used!
-		$post_types = apply_filters( 'beautiful_filters_post_types', get_option('beautiful_taxonomy_filters_post_types') ); 
-		$excluded_taxonomies = apply_filters( 'beautiful_filters_taxonomies', get_option('beautiful_taxonomy_filters_taxonomies') ); 
+		$post_types = apply_filters( 'beautiful_filters_post_types', get_option('beautiful_taxonomy_filters_post_types') );
+		$excluded_taxonomies = apply_filters( 'beautiful_filters_taxonomies', get_option('beautiful_taxonomy_filters_taxonomies') );
 
 		//If there's no post types, bail early!
-		if(!$post_types) 
+		if(!$post_types)
 			return;
 
 		//get current post type archive
@@ -206,7 +217,7 @@ class Beautiful_Taxonomy_Filters_Public {
 				}else{
 					global $wp_query;
 					if($wp_query->query['post_type'] != ''){
-						$current_post_type = $wp_query->query['post_type'];	
+						$current_post_type = $wp_query->query['post_type'];
 					}
 				}
 			}
@@ -227,22 +238,22 @@ class Beautiful_Taxonomy_Filters_Public {
 
 			require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/beautiful-taxonomy-filters-public-display.php';
 		}
-		
+
 	}
-	
+
 	/**
 	* Public function to return information about the currently active filters
 	*
 	* @since 1.0.0
 	*/
 	public static function beautiful_filters_info(){
-		
+
 		global $wp_query;
 		//as long as we have some taxonomies, lets show the info!
 		if(!empty($wp_query->tax_query->queries)){
-			require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/beautiful-taxonomy-filters-public-info-display.php';	
+			require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/beautiful-taxonomy-filters-public-info-display.php';
 		}
-		
+
 	}
 
 }
